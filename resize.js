ac7a870e961fcbcd0e5f362d6678df27f6a6d7db
George Stagas <gstagas@gmail.com>
2015-06-13 13:41:10 +0300

    add `no-resize` directive

--- resize.js
+++ resize.js
@@ -1,8 +1,10 @@
 
 var join = require('path').posix.join;
+var exists = require('fs').existsSync;
 var retina = require('./util').retina;
 var async = require('async');
 var gm = require('gm');
+var cp = require('cp');
 
 module.exports = resize;
 
@@ -17,17 +19,26 @@ function resize(dir, targets) {
 
       async.series({
         regular: function(fn) {
-          imageResize(target.src, dest, target.size, fn);
+          imageResize(target.src, dest, target.size, false, fn);
         },
         retina: function(fn) {
-          imageResize(target.src, retina(dest), retina(target.size), fn);
+          imageResize(target.src, dest, target.size, true, fn);
         }
       }, fn);
     }, fn);
   };
 }
 
-function imageResize(src, dest, size, fn) {
+function imageResize(src, dest, size, toRetina, fn) {
+  if ('no-resize' === size[0]) {
+    return copy(src, dest, toRetina, fn);
+  }
+
+  if (toRetina) {
+    dest = retina(dest);
+    size = retina(size);
+  }
+
   gm(src)
   .noProfile()
   .gravity('Center')
@@ -36,4 +47,30 @@ function imageResize(src, dest, size, fn) {
   .resize(size[0], size[1], '^')
   .extent(size[0], size[1])
   .write(dest, fn);
+}
+
+function copy(src, dest, toRetina, fn) {
+  var hasRetina = exists(retina(src));
+
+  if (toRetina) {
+    dest = retina(dest);
+
+    if (hasRetina) {
+      src = retina(src);
+    }
+
+    cp(src, dest, fn);
+  } else {
+    if (hasRetina) {
+      cp(src, dest, fn);
+    } else {
+      gm(src)
+      .noProfile()
+      .gravity('Center')
+      .background('transparent')
+      .density(1200, 1200)
+      .resize(50, 50, '%')
+      .write(dest, fn);
+    }
+  }
 }
